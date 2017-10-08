4ccb6f6b3af3dae64f9a036ee5e8d7fa1dcc05b1
natthapach <natthapach.a@ku.th>
2017-10-09 00:35:07 +0700

    initital commit

---
+++ public/js/firebase.js
@@ -0,0 +1,13 @@
+<script src="https://www.gstatic.com/firebasejs/4.1.1/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBb14iuaDPstpfQhLR1eoklvSPFGRTxEBE",
+    authDomain: "test1-3328f.firebaseapp.com",
+    databaseURL: "https://test1-3328f.firebaseio.com",
+    projectId: "test1-3328f",
+    storageBucket: "test1-3328f.appspot.com",
+    messagingSenderId: "589181130811"
+  };
+  firebase.initializeApp(config);
+</script>
